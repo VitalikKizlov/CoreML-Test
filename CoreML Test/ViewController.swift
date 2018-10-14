27f3375414c4b1012ffff5de617796c1aa0259eb
Vitalik Kizlov <vitalik2602@gmail.com>
2018-10-14 13:25:36 +0300

    Initial Commit

---
+++ CoreML Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreML Test
+//
+//  Created by Vitalik Kizlov on 10/14/18.
+//  Copyright © 2018 Vitalik Kizlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
